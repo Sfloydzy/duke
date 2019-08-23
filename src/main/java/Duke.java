874088c957b5ebdf8a1b9a637163ce5093f6b03f
Sfloydzy <e0310657@u.nus.edu>
2019-08-23 12:24:51 +0800

    Level 2 Completed

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,14 @@
+import java.util.ArrayList;
 import java.util.Scanner;
+
 
 public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Duke\n" +
                 "What can I do for you?");
 
+        ArrayList<String> a = new ArrayList<>();
+        int cnt = 0;
         while(true) {
             Scanner sc = new Scanner(System.in);
             String input = sc.next();
@@ -13,8 +17,16 @@ public class Duke {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             }
+
+            else if (input.equals("list")) {
+                for (String s: a) {
+                    System.out.println(s);
+                }
+            }
             else {
-                System.out.println(input);
+                ++cnt;
+                a.add(cnt + ". " + input);
+                System.out.println("added: " + input);
             }
         }
 
