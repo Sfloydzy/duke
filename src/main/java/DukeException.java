7e9e790fe6744e43de30888526d84af81998c1d2
Sfloydzy <e0310657@u.nus.edu>
2019-08-27 12:45:19 +0800

    Tidying

--- src/main/java/DukeException.java
+++
@@ -1,8 +0,0 @@
-import  java.lang.Exception;
-import java.lang.StringIndexOutOfBoundsException;
-
-public class DukeException extends Exception {
-    public DukeException (String message) {
-        System.out.println(message);
-    }
-}
